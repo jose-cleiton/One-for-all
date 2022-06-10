c6067fc1da7dcdd78955ceedfcb0abb20f14d1a0
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-06-10 09:42:43 -0300

    desafio2 corrigido

--- desafio2.sql
+++ desafio2.sql
@@ -1,4 +1,11 @@
-SELECT   *
+SELECT 
+    COUNT(DISTINCT(M.id)) AS cancoes,
+    COUNT(DISTINCT(A.id)) AS artistas,
+    COUNT(DISTINCT(AL.id)) AS albums
+FROM SpotifyClone.artistas AS A
+  INNER JOIN SpotifyClone.album  AS AL
+    ON  A.id = AL.artistas_id
+  INNER JOIN SpotifyClone.musica AS M   
+    ON  AL.id = M.album_id
 
-FROM SpotfyClone.artists
-
+SELECT id,nome,data_de_lancamento,artistas_id FROM album;
