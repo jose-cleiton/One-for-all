cd828c39356ef253cab02caea6a4f8147e40cd14
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-06-25 18:09:53 -0300

    Feat: Desafio 09 Dnovo

--- desafio9.sql
+++ desafio9.sql
@@ -1,8 +1,9 @@
 SELECT 
-  COUNT(H.musica_id) AS quantidade_musicas_no_historico
+  COUNT(*) AS quantidade_musicas_no_historico
 
 FROM spotifyclone.usuario AS U
 
 INNER JOIN spotifyclone.historico_de_reproducoes H 
 ON U.hist_id = H.usuario_id 
 WHERE U.nome LIKE "Bill";
+
