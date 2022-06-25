d9bf02191095a181bd5f5b7ba3d8ea659bd08669
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-06-25 16:44:49 -0300

    Dnovo

--- desafio5.sql
+++ desafio5.sql
@@ -2,10 +2,8 @@ SELECT M.nome AS cancao,
   COUNT(H.usuario_id) AS reproducoes
 
 FROM spotifyclone.musica AS M 
-INNER JOIN spotifyclone.historico_de_reproducoes 
-AS H ON M.id = H.musica_id
-
-
+INNER JOIN spotifyclone.historico_de_reproducoes AS H 
+ON M.id = H.musica_id
 GROUP BY
   cancao
 ORDER BY
