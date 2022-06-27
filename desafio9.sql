e607ec7b2a3745140e6747c1c1334e18dfba3e21
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-06-27 13:33:35 -0300

    Colocando as letras camelcase

--- desafio9.sql
+++ desafio9.sql
@@ -1,6 +1,7 @@
 SELECT COUNT(*) AS quantidade_musicas_no_historico
-FROM spotifyclone.historico_de_reproducoes H 
-INNER JOIN spotifyclone.usuario AS U
+
+FROM SpotifyClone.historico_de_reproducoes H 
+INNER JOIN SpotifyClone.usuario AS U
 ON U.hist_id = H.usuario_id 
 WHERE U.nome LIKE "Bill";
 
